ffd70e1158bb330cdfd8646b3b3abafade0a385e
Francesco Pogliano <54809768+Cyangray@users.noreply.github.com>
2024-10-23 10:10:34 +0200

    Model for user info created

--- src/application.py
+++ src/application.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
+from pydantic import BaseModel, PositiveInt, EmailStr, StringConstraints
+
+# from typing import Literal
+from typing_extensions import Literal, Annotated, Optional
+import datetime
 
 app = FastAPI()
+
+
+class User(BaseModel):
+    user_id: PositiveInt
+    username: Annotated[str, StringConstraints(min_length=2, pattern=r"^[a-zA-Z]*$")]
+    email: EmailStr
+    age: Optional[PositiveInt]
+    country: Optional[Annotated[str, StringConstraints(pattern=r"^[A-Z]{2}$")]]
+
+
+class SuperUser(User):
+    superuser_id: PositiveInt
+    role: Literal["admin", "moderator", "support"]
+
+
+class Activity(BaseModel):
+    activity_id: PositiveInt
+    time: datetime.datetime
+    user_id: PositiveInt
+    activity_type: Literal["click", "login", "logout", "purchase"]
+    activity_details: str
 
 
 @app.get("/", response_class=PlainTextResponse)
--- test/test_application.py
+++ test/test_application.py
@@ -1,6 +1,8 @@
 from fastapi.testclient import TestClient
-
-from src.application import app
+from pydantic import ValidationError
+from src.application import app, User, SuperUser, Activity
+import pytest
+from datetime import datetime, timezone
 
 
 def test_startup():
@@ -8,3 +10,76 @@ def test_startup():
         response = client.get("/")
         assert response.status_code == 200
         assert response.text == "Hello, I'm good!"
+
+
+# TODO: read up about monkeypatch again, I'm probably doing things wrong here. Check https://stackoverflow.com/questions/20503373/how-to-monkeypatch-pythons-datetime-datetime-now-with-py-test
+
+
+def test_valid_user():
+    mock_data_user = {
+        "user_id": 1,
+        "username": "Pippo",
+        "email": "aaa@bbb.cc",
+        "age": 23,
+        "country": "UK",
+    }
+    assert User(**mock_data_user).model_dump() == mock_data_user
+
+
+def test_valid_superuser():
+    mock_data_superuser = {
+        "user_id": 1,
+        "username": "Pippo",
+        "email": "aaa@bbb.cc",
+        "age": 23,
+        "country": "UK",
+        "superuser_id": 1,
+        "role": "moderator",
+    }
+    assert SuperUser(**mock_data_superuser).model_dump() == mock_data_superuser
+
+
+def test_valid_activity():
+    mock_data_activity = {
+        "activity_id": 1,
+        "time": datetime(2024, 10, 23, 9, 41, 3, tzinfo=timezone.utc),
+        "user_id": 3,
+        "activity_type": "login",
+        "activity_details": "First login of the day",
+    }
+    assert Activity(**mock_data_activity).model_dump() == mock_data_activity
+
+
+def test_invalid_user_data():
+    with pytest.raises(ValidationError):
+        User(user_id=-1)
+        User(user_id="1")
+        User(username=12)
+        User(username="Pippo333")
+        User(email="sssss")
+        User(age="ii")
+        User(age=-22)
+        User(country="UKee")
+
+
+def test_invalid_superuser_data():
+    with pytest.raises(ValidationError):
+        SuperUser(superuser_id=-1)
+        SuperUser(superuser_id="1")
+        SuperUser(role="Admin")
+        SuperUser(role="Pippo")
+        SuperUser(role="")
+        SuperUser(role=33)
+
+
+def test_invalid_activity():
+    with pytest.raises(ValidationError):
+        Activity(activity_id=-1)
+        Activity(activity_id="1")
+        # TODO: add datetime test
+        Activity(user_id=-1)
+        Activity(user_id="1")
+        Activity(activity_type="Login")
+        Activity(activity_type="Pippo")
+        Activity(activity_type="")
+        Activity(activity_details=33)
