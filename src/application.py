a378ee1f058f61996bd99f4bc568e4d10b6da898
Francesco Pogliano <54809768+Cyangray@users.noreply.github.com>
2024-10-23 14:15:24 +0200

    Datetime validation fixed to not accept integers

--- src/application.py
+++ src/application.py
@@ -1,15 +1,21 @@
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
-from pydantic import BaseModel, PositiveInt, EmailStr, StringConstraints
+from pydantic import (
+    BaseModel,
+    PositiveInt,
+    EmailStr,
+    StringConstraints,
+    field_validator,
+)
 
 # from typing import Literal
 from typing_extensions import Literal, Annotated, Optional
-import datetime
+from datetime import datetime
 
 app = FastAPI()
 
 
-class User(BaseModel):
+class User(BaseModel, validate_assignment=True):
     user_id: PositiveInt
     username: Annotated[str, StringConstraints(min_length=2, pattern=r"^[a-zA-Z]*$")]
     email: EmailStr
@@ -17,17 +23,23 @@ class User(BaseModel):
     country: Optional[Annotated[str, StringConstraints(pattern=r"^[A-Z]{2}$")]]
 
 
-class SuperUser(User):
+class SuperUser(User, validate_assignment=True):
     superuser_id: PositiveInt
     role: Literal["admin", "moderator", "support"]
 
 
-class Activity(BaseModel):
+class Activity(BaseModel, validate_assignment=True):
     activity_id: PositiveInt
-    time: datetime.datetime
+    time: datetime
     user_id: PositiveInt
     activity_type: Literal["click", "login", "logout", "purchase"]
     activity_details: str
+
+    @field_validator("time", mode="before")
+    def no_int_to_datetime(cls, entry):
+        if isinstance(entry, int):
+            raise ValueError("Integers are not valid datetime entries")
+        return entry
 
 
 @app.get("/", response_class=PlainTextResponse)
--- test/test_application.py
+++ test/test_application.py
@@ -4,6 +4,32 @@ from src.application import app, User, SuperUser, Activity
 import pytest
 from datetime import datetime, timezone
 
+mock_data_user = {
+    "user_id": 1,
+    "username": "Pippo",
+    "email": "aaa@bbb.cc",
+    "age": 23,
+    "country": "UK",
+}
+
+mock_data_superuser = {
+    "user_id": 1,
+    "username": "Pippo",
+    "email": "aaa@bbb.cc",
+    "age": 23,
+    "country": "UK",
+    "superuser_id": 1,
+    "role": "moderator",
+}
+
+mock_data_activity = {
+    "activity_id": 1,
+    "time": datetime(2024, 10, 23, 9, 41, 3, tzinfo=timezone.utc),
+    "user_id": 3,
+    "activity_type": "login",
+    "activity_details": "First login of the day",
+}
+
 
 def test_startup():
     with TestClient(app) as client:
@@ -13,76 +39,65 @@ def test_startup():
 
 
 def test_valid_user():
-    mock_data_user = {
-        "user_id": 1,
-        "username": "Pippo",
-        "email": "aaa@bbb.cc",
-        "age": 23,
-        "country": "UK",
-    }
     assert User(**mock_data_user).model_dump() == mock_data_user
 
 
 def test_valid_superuser():
-    mock_data_superuser = {
-        "user_id": 1,
-        "username": "Pippo",
-        "email": "aaa@bbb.cc",
-        "age": 23,
-        "country": "UK",
-        "superuser_id": 1,
-        "role": "moderator",
-    }
     assert SuperUser(**mock_data_superuser).model_dump() == mock_data_superuser
 
 
 def test_valid_activity():
-    mock_data_activity = {
-        "activity_id": 1,
-        "time": datetime(2024, 10, 23, 9, 41, 3, tzinfo=timezone.utc),
-        "user_id": 3,
-        "activity_type": "login",
-        "activity_details": "First login of the day",
-    }
     assert Activity(**mock_data_activity).model_dump() == mock_data_activity
 
 
 def test_invalid_user_data():
-    with pytest.raises(ValidationError):
-        User(user_id=-1)
-        User(user_id="1")
-        User(username=12)
-        User(username="Pippo333")
-        User(email="sssss")
-        User(age="ii")
-        User(age=-22)
-        User(country="UKee")
+    user = User(**mock_data_user)
 
-
-def test_invalid_superuser_data():
     with pytest.raises(ValidationError):
-        SuperUser(superuser_id=-1)
-        SuperUser(superuser_id="1")
-        SuperUser(role="Admin")
-        SuperUser(role="Pippo")
-        SuperUser(role="")
-        SuperUser(role=33)
+        user.user_id = -1
+    with pytest.raises(ValidationError):
+        user.username = 12
+    with pytest.raises(ValidationError):
+        user.username = "Pippo333"
+    with pytest.raises(ValidationError):
+        user.email = "sssss"
+    with pytest.raises(ValidationError):
+        user.age = "ii"
+    with pytest.raises(ValidationError):
+        user.age = -22
+    with pytest.raises(ValidationError):
+        user.country = "UKee"
 
 
 def test_invalid_activity():
+    activity = Activity(**mock_data_activity)
     with pytest.raises(ValidationError):
-        Activity(activity_id=-1)
-        Activity(activity_id="1")
-        Activity(
-            time=datetime(2024, 10, 23, 9, 41, 3, tzinfo=timezone.utc).isoformat(
-                timespec="seconds"
-            )
-        )
-        Activity(time=-3)
-        Activity(time="Pippo")
-        Activity(user_id=-1)
-        Activity(user_id="1")
-        Activity(activity_type="Login")
-        Activity(activity_type="Pippo")
-        Activity(activity_type="")
-        Activity(activity_details=33)
+        activity.activity_id = -1
+    with pytest.raises(ValidationError):
+        activity.time = -3
+    with pytest.raises(ValidationError):
+        activity.time = "Pippo"
+    with pytest.raises(ValidationError):
+        activity.user_id = -1
+    with pytest.raises(ValidationError):
+        activity.activity_type = "Login"
+    with pytest.raises(ValidationError):
+        activity.activity_type = "Pippo"
+    with pytest.raises(ValidationError):
+        activity.activity_type = ""
+    with pytest.raises(ValidationError):
+        activity.activity_details = 33
+
+
+def test_invalid_superuser_data():
+    superuser = SuperUser(**mock_data_superuser)
+    with pytest.raises(ValidationError):
+        superuser.superuser_id = -1
+    with pytest.raises(ValidationError):
+        superuser.role = "Admin"
+    with pytest.raises(ValidationError):
+        superuser.role = "Pippo"
+    with pytest.raises(ValidationError):
+        superuser.role = ""
+    with pytest.raises(ValidationError):
+        superuser.role = 33
