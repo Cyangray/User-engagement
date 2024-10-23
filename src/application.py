0d89f0da7e7752d2a93f44c19ccb29f66819e9e4
Francesco Pogliano <54809768+Cyangray@users.noreply.github.com>
2024-10-23 16:36:09 +0200

    Update application.py

--- src/application.py
+++ src/application.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
@@ -7,3 +7,35 @@ app = FastAPI()
 @app.get("/", response_class=PlainTextResponse)
 async def root():
     return "Hello, I'm good!"
+
+
+users_id_db = [
+    i for i in range(1, 100)
+]  # TODO: placeholder. This should be created automatically when instantiating objects
+superusers_id_db = [
+    i for i in range(1, 100)
+]  # TODO: placeholder. This should be created automatically when instantiating objects
+activities_id_db = [
+    i for i in range(1, 100)
+]  # TODO: placeholder. This should be created automatically when instantiating objects
+activities_db = []
+
+
+@app.post("/activities/")
+async def create_activity(item: Activity):
+    activities_db.append(item)
+    return item
+
+
+@app.get("/activities/")
+async def read_activities_by_userid(user_id: PositiveInt):
+    if user_id not in users_id_db:
+        raise HTTPException(status_code=404, detail="User not found.")
+    filtered_activities: list[Activity] = [
+        activity for activity in activities_db if activity.user_id == user_id
+    ]
+    if len(filtered_activities) == 0:
+        raise HTTPException(
+            status_code=404, detail=f"No activities by {user_id=} found."
+        )
+    return filtered_activities
