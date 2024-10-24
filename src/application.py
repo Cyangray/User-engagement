57be78adb652c81e940a77fd32a681aa57130a5f
Francesco Pogliano <54809768+Cyangray@users.noreply.github.com>
2024-10-24 16:22:05 +0200

    Added read/write/search, tests to be added

--- src/application.py
+++ src/application.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import PlainTextResponse
+from pydantic import PositiveInt, EmailStr
+
+from src.models import User, Activity, ActivityTypes, create_activity, create_user
 
 app = FastAPI()
 
@@ -9,20 +12,36 @@ async def root():
     return "Hello, I'm good!"
 
 
-users_id_db = [
-    i for i in range(1, 100)
-]  # TODO: placeholder. This should be created automatically when instantiating objects
 superusers_id_db = [
     i for i in range(1, 100)
 ]  # TODO: placeholder. This should be created automatically when instantiating objects
-activities_id_db = [
-    i for i in range(1, 100)
-]  # TODO: placeholder. This should be created automatically when instantiating objects
+
+users_db = []
+emails_db = []
+users_id_db = []
 activities_db = []
+activities_id_db = []
+
+
+@app.post("/users/")
+async def post_user(username, email: EmailStr, age=None, country=None) -> User:
+    if email in emails_db:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    item = create_user(username, email, age, country)
+    users_id_db.append(item.user_id)
+    users_db.append(item)
+    emails_db.append(email)
+    return item
 
 
 @app.post("/activities/")
-async def create_activity(item: Activity):
+async def post_activity(
+    user_id: PositiveInt, activity_type: ActivityTypes, activity_details: str
+) -> Activity:
+    if user_id not in users_id_db:
+        raise HTTPException(status_code=404, detail="User ID not found")
+    item = create_activity(user_id, activity_type, activity_details)
+    activities_id_db.append(item.activity_id)
     activities_db.append(item)
     return item
 
@@ -30,7 +49,7 @@ async def create_activity(item: Activity):
 @app.get("/activities/")
 async def read_activities_by_userid(user_id: PositiveInt):
     if user_id not in users_id_db:
-        raise HTTPException(status_code=404, detail="User not found.")
+        raise HTTPException(status_code=404, detail="User ID not found.")
     filtered_activities: list[Activity] = [
         activity for activity in activities_db if activity.user_id == user_id
     ]
