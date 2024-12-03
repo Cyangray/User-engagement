bacc3ce59925f87c1ea48d3add8e8467b954c54f
Francesco Pogliano <54809768+Cyangray@users.noreply.github.com>
2024-12-03 16:10:54 +0100

    prettified code

--- devtools/generate_dataset.py
+++ devtools/generate_dataset.py
@@ -14,20 +14,21 @@ CLICKS_PER_MINUTE = 3
 SESSION_LENGTH_HOURS = 2
 SESSIONS_PER_YEAR = 5
 
-connection_manager = get_db()
-cursor = connection_manager.connection.cursor()
-cursor.execute(create_test_tables())
-# generate users
-users = []
-for n in range(N_USERS):
-    fake_user = generate_fake_user()
-    user = post_fake_user_to_DB(cursor, fake_user)
-    users.append(user)
+if __name__ == "__main__":
+    connection_manager = get_db()
+    cursor = connection_manager.connection.cursor()
+    cursor.execute(create_test_tables())
+    # generate users
+    users = []
+    for n in range(N_USERS):
+        fake_user = generate_fake_user()
+        user = post_fake_user_to_DB(cursor, fake_user)
+        users.append(user)
 
-for user in users:
-    dates = generate_dates(sessions_per_year=SESSIONS_PER_YEAR)
-    for date in dates:
-        list_of_fake_activities_in_session = generate_session(
-            date, user.user_id, CLICKS_PER_MINUTE, SESSION_LENGTH_HOURS
-        )
-        post_session(cursor, list_of_fake_activities_in_session)
+    for user in users:
+        dates = generate_dates(sessions_per_year=SESSIONS_PER_YEAR)
+        for date in dates:
+            list_of_fake_activities_in_session = generate_session(
+                date, user.user_id, CLICKS_PER_MINUTE, SESSION_LENGTH_HOURS
+            )
+            post_session(cursor, list_of_fake_activities_in_session)
